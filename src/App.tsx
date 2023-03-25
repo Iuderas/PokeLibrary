c53bd248933c0e322ddfce08fd9feedcee9f350d
Alvaro Makoto <amakoto@doris.mobi>
2023-03-25 20:07:36 -0300

    feat: create pokemon selected screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Pokedex } from "./components/pokedex";
+// import { Pokedex } from "./components/Pokedex";
+import { SelectedPokemon } from "./screens/SelectedPokemon";
 
 export const App: React.FC = () => {
   return (
     <>
-      <Pokedex />
+      {/* <Pokedex /> */}
+      <SelectedPokemon />
     </>
   );
 };
---
+++ src/screens/SelectedPokemon/index.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { usePokemonsInfo } from "../../hooks/getPokemons";
+import * as Styled from "./styles";
+
+export const SelectedPokemon: React.FC = () => {
+  const { getPokemonDetails, currentPokemon } = usePokemonsInfo();
+
+  useEffect(() => {
+    getPokemonDetails({ url: "https://pokeapi.co/api/v2/pokemon/articuno" });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Styled.Wrapper>
+      <Styled.Container>
+        <Styled.WrapperImage>
+          <Styled.Sprite
+            src={currentPokemon?.sprites.front_default as string}
+            alt="pokemon-display"
+          />
+        </Styled.WrapperImage>
+        <Styled.Label>{currentPokemon?.name}</Styled.Label>
+
+        <Styled.DescriptionWrapper>
+          <Styled.Button>types</Styled.Button>
+          <Styled.Button>moves</Styled.Button>
+          <Styled.Button>evolution</Styled.Button>
+          <Styled.Button>weakness</Styled.Button>
+          <Styled.Button>resistances</Styled.Button>
+        </Styled.DescriptionWrapper>
+      </Styled.Container>
+    </Styled.Wrapper>
+  );
+};
